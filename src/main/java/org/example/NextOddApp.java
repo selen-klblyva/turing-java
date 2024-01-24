0f81ef59fa8061cfba9860e000a92e228678bec6
Selen <kalbaliyevaselen@gmail.com>
2024-01-24 16:49:48 +0400

    problem \

---
+++ src/main/java/org/example/NextOddApp.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.Scanner;
+public class NextOddApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n= scanner.nextInt();
+
+        if(n%2==0){
+            int num=n+1;
+            System.out.println(num);
+        }
+        else{
+            int num=n+2;
+            System.out.println(num);
+        }
+
+
+
+
+    }
+}

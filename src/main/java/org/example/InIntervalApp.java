58b47120c9ead0adf84fd7c8c47ba0db53a67d01
Selen <kalbaliyevaselen@gmail.com>
2024-01-24 13:26:07 +0400

    problem T

---
+++ src/main/java/org/example/InIntervalApp.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Scanner;
+public class InIntervalApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x= scanner.nextInt();
+        int a= scanner.nextInt();
+        int b= scanner.nextInt();
+
+        if(x>=a && x<=b){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}

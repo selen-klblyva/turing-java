a104220978f9e9415cb39692c707e3dd767418e2
Selen <kalbaliyevaselen@gmail.com>
2024-01-29 19:36:22 +0400

    problem1

--- src/main/java/game/code/CircleApp.java
+++ src/main/java/game/code/CircleApp.java
@@ -1,21 +1,18 @@
+package game.code;
 
 import java.util.Scanner;
 
-import java.util.Scanner;
 
-public class SquareApp {
+public class CircleApp {
     public static void main(String[] args) {
 
-    /* int a=5;
-    System.out.println(a%2==0);
 
-     */
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int r = scanner.nextInt();
 
-        System.out.println(2*Math.PI*r);
-        System.out.println(Math.PI*Math.pow(r,2));
+        System.out.println(2 * Math.PI * r);
+        System.out.println(Math.PI * Math.pow(r, 2));
 
 
     }

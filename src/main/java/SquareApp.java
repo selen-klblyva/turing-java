791e9a876dd5a31c011079b6c06cc0325405a030
Selen <kalbaliyevaselen@gmail.com>
2024-01-29 19:29:48 +0400

    problem last

---
+++ src/main/java/SquareApp.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class SquareApp {
+    public static void main(String[] args) {
+
+    /* int a=5;
+    System.out.println(a%2==0);
+
+     */
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(4 * n);
+        System.out.println(n * n);
+
+
+    }
+}
